5ab691a323607f75966d51a11711f8ad56d4454b
Sadzak23 <sadzak23@gmail.com>
2020-10-31 21:34:14 +0100

    Create list of Gists in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { Gist } from "./components/Gist";
 
-function App() {
+interface GistType {
+  owner: {
+    avatar_url: string;
+  };
+  id: string;
+  files: {
+    [key: string]: {
+      filename: string;
+    };
+  };
+}
+
+const App = () => {
+  const [pageNo, setPageNo] = useState(1);
+  const [gists, setGists] = useState<GistType[]>([]);
+  const [activeGistId, setActiveGistId] = useState<string>();
+
+  useEffect(() => {
+    const getGists = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.github.com/gists/public?per_page=30&page=${pageNo}`
+        );
+        await setGists(response.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getGists();
+  }, [pageNo]);
+
+  const onGistClick = (id: string) => {
+    setActiveGistId(id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="app-header">
+        <h2>Gists</h2>
       </header>
+      <div className="app-content">
+        {gists.map((gist) => (
+            <Gist
+              key={gist.id}
+              avatarUrl={gist.owner.avatar_url}
+              fileName={Object.keys(gist.files)[0]}
+              onGistClick={() => onGistClick(gist.id)}
+              isActive={gist.id === activeGistId}
+            />
+          ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
