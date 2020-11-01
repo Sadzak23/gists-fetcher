8a05cd43f027b6b9df6ccea0993bbed744f65521
Sadzak23 <sadzak23@gmail.com>
2020-11-01 18:59:26 +0100

    Create Gist Modal

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { Gist } from "./components/Gist";
 import { Loading } from "./components/Loading";
 import { Pagination } from "./components/Pagination";
+import { Modal } from "./components/Modal";
 
 interface GistType {
   owner: {
@@ -16,57 +17,69 @@ interface GistType {
     };
   };
 }
+interface ActiveGist {
+  id: string;
+  avatarUrl: string;
+}
 
 const App = () => {
   const [pageNo, setPageNo] = useState(1);
   const [gists, setGists] = useState<GistType[]>([]);
-  const [activeGistId, setActiveGistId] = useState<string>();
+  const [activeGist, setActiveGist] = useState<ActiveGist>();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
 
-  
+  const modalRef = React.createRef<HTMLDivElement>();
+
+  // Fetch gists on page change
   useEffect(() => {
     const url = `https://api.github.com/gists/public?per_page=30&page=${pageNo}`;
     const getGists = async () => {
       setIsLoading(true);
+      setActiveGist(undefined);
       try {
         const response = await axios.get(url);
-        await setGists(response.data);
+        setGists(response.data);
+        window.scrollTo(0, 0); // Scroll to top
       } catch (error) {
         setError(error.message);
-      } finally {        
-        window.scrollTo(0, 0);    // Scroll to top
+      } finally {
         setIsLoading(false);
       }
     };
-    getGists();   // Fetch Gists
+    getGists();
   }, [pageNo]);
 
-  const onGistClick = (id: string) => {
-    setActiveGistId(id);
+  // Show modal on gist click
+  useEffect(() => {
+    modalRef.current?.classList.add("modal-visible");
+    setTimeout(() => modalRef.current?.classList.remove("modal-visible"), 1000);
+  }, [activeGist, modalRef]);
+
+  const onGistClick = (id: string, avatarUrl: string) => {
+    setActiveGist({ id, avatarUrl });
   };
 
   const Content = () => {
     if (error) {
-      return <p className="error-msg">Error: {error}</p>
+      return <p className="error-msg">Error: {error}</p>;
     }
-    else if (isLoading) {
-      return <Loading />
-    } else {
-      return gists.map((gist) => (
-        <Gist
-          key={gist.id}
-          avatarUrl={gist.owner.avatar_url}
-          fileName={Object.keys(gist.files)[0]}
-          onGistClick={() => onGistClick(gist.id)}
-          isActive={gist.id === activeGistId}
-        />
-      ))
+    if (isLoading) {
+      return <Loading />;
     }
+    return gists.map((gist) => (
+      <Gist
+        key={gist.id}
+        avatarUrl={gist.owner.avatar_url}
+        fileName={Object.keys(gist.files)[0]}
+        onGistClick={() => onGistClick(gist.id, gist.owner.avatar_url)}
+        isActive={gist.id === activeGist?.id}
+      />
+    ));
   };
 
   return (
-    <div>
+    <React.Fragment>
       <header className="app-header">
         <h2>Gists</h2>
       </header>
@@ -74,7 +87,10 @@ const App = () => {
         {Content()}
         {!error && <Pagination pageNo={pageNo} setPageNo={setPageNo} />}
       </div>
-    </div>
+      {!!activeGist && (
+        <Modal modalRef={modalRef} avatarUrl={activeGist?.avatarUrl} />
+      )}
+    </React.Fragment>
   );
 };
 
